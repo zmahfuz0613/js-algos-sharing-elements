43b8a16a220069f3e4f18b8fcc5b0ed69a42e7e7
Misha Kessler <misha.kessler@generalassemb.ly>
2020-04-08 10:44:23 -0400

    Update test.js

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,14 +1,14 @@
 let algos = require("../sharingElements.js")
 
 test("shares the array elements with new arrays consisting of a maximum of num", () => {
-  expect(algos.groupArrays(["a", "b", "c", "d"], 4)).toMatchObject([
+  expect(algos.sharingElements(["a", "b", "c", "d"], 4)).toMatchObject([
     ["a", "b", "c", "d"]
   ])
-  expect(algos.groupArrays(["a", "b", "c", "d"], 3)).toMatchObject([
+  expect(algos.sharingElements(["a", "b", "c", "d"], 3)).toMatchObject([
     ["a", "b", "c"],
     ["d"]
   ])
-  expect(algos.groupArrays(["a", "b", "c", "d"], 2)).toMatchObject([
+  expect(algos.sharingElements(["a", "b", "c", "d"], 2)).toMatchObject([
     ["a", "b"],
     ["c", "d"]
   ])
