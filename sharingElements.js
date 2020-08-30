afb0509d17d684f21bb650225e22b180a9044917
Zunayed Mahfuz <zunayed.eshna@gmail.com>
2020-08-30 11:59:37 -0400

    Completed

--- sharingElements.js
+++ sharingElements.js
@@ -1,7 +1,16 @@
 //SHARING ELEMENTS
 
 const sharingElements = (arr, size) => {
+  var result = arr.reduce(function(newArr, element, index){
+    const chunkIndex = Math.floor(index/size);
+    if(!newArr[chunkIndex]) {
+      newArr[chunkIndex] = [];
+    }  
+    newArr[chunkIndex].push(element);
+    return newArr;
+  }, []);
 
+  return result;
 }
 
 console.log(sharingElements(["a", "b", "c", "d"], 3))
